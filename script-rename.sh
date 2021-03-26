ffcb7c28678baea29319727c21d69fe1902e2f02
Uri Herrera <hello@uriherrera.com>
2021-03-26 13:45:27 -0600

    [ci skip]

--- script-rename.sh
+++ script-rename.sh
@@ -15,7 +15,7 @@ find src-rename/ -type f -exec sed -i 's/lightly/nitrux/g' {} +
 find src-rename/ -type f -exec sed -i 's/Lightly/Nitrux/g' {} +
 find src-rename/ -type f -exec sed -i 's/LIGHTLY/NITRUX/g' {} +
 
-cd src-rename-build/
+cd src-rename/build/
 
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_TESTING=OFF ..
 make
