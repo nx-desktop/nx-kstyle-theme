d7b31b67002b78655c3ba4f49813522578173de6
Uri Herrera <hello@uriherrera.com>
2021-06-30 04:01:58 -0500

    upload to packagecloud

--- travis-build.sh
+++ travis-build.sh
@@ -16,7 +16,8 @@ apt -qq update
 
 ### Install Dependencies
 
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install devscripts lintian build-essential automake autotools-dev rename
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install  devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules rename
+
 mk-build-deps -i -t "apt-get --yes" -r
 
 ###	Rename files, folders and change strings
